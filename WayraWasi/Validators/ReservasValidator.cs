a891bb515cbc53648d165552ded2baef450caaa4
LionelBossio <matiasbossio101@gmail.com>
2024-07-31 19:41:13 -0300

    - Errores corregidos en el crear de reservas. - Agregado metodo para validar fechas en en validador de reservas - Modificacion en validador de reservas a la hora de ver la capacidad de las cabañas.

--- WayraWasi/Validators/ReservasValidator.cs
+++ WayraWasi/Validators/ReservasValidator.cs
@@ -18,10 +18,12 @@ namespace WayraWasi.Validators
 
             RuleFor(r => r.FechaEntrada)
                 .NotNull().WithMessage("La fecha de entrada es obligatoria.")
+                .Must(FechaValida).WithMessage("La fecha de entrada no puede ser anterior a la fecha actual.")
                 .LessThan(r => r.FechaSalida).WithMessage("La fecha de entrada debe ser anterior a la fecha de salida.");
 
             RuleFor(r => r.FechaSalida)
-                .NotNull().WithMessage("La fecha de salida es obligatoria.");
+                .NotNull().WithMessage("La fecha de salida es obligatoria.")
+                .Must(FechaValida).WithMessage("La fecha de salida no puede ser anterior a la fecha actual.");
 
             RuleFor(r => r.NumeroPersonas)
                 .NotEmpty().WithMessage("La cantida de personas es obligatorio.")
@@ -32,9 +34,15 @@ namespace WayraWasi.Validators
             RuleFor(reserva => reserva)
             .Must(DisponibilidadCabaña).WithMessage("La cabaña ya se encuentra reservada en esas fechas.");
 
-            RuleFor(reserva => reserva.NumeroPersonas)
-                .Must(CapacidadMaxima)
-                .WithMessage(reserva => $"El número de personas excede la capacidad de la cabaña seleccionada. La capacidad máxima es de {ObtenerCapacidadCabania(reserva.IdCabania)}.");
+            RuleFor(reserva => reserva)
+                .Must((reserva, token) => CapacidadMaxima(reserva, reserva.NumeroPersonas))
+                .WithMessage(reserva => $"El número de personas excede la capacidad de la cabaña seleccionada. La capacidad máxima es de {ObtenerCapacidadCabania(reserva.IdCabania)}.")
+                .When(reserva => reserva.IdCabania > 0);
+        }
+
+        private bool FechaValida(DateTime? date)
+        {
+            return date.HasValue && date.Value.Date >= DateTime.Today;
         }
 
         private bool DisponibilidadCabaña(Reserva reserva)
@@ -56,7 +64,12 @@ namespace WayraWasi.Validators
         private long ObtenerCapacidadCabania(int idCabania)
         {
             Cabania cabaniaCapacidad = _repository.BuscarPorIDCabania(idCabania).Result;
+            if (cabaniaCapacidad == null)
+            {
+                return 0;
+            }
             return cabaniaCapacidad.Capacidad;
+
         }
     }
 }
